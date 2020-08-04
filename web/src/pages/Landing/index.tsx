ea8f85d402d32e8d56c52bceb9dd362ccbb9efa6
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-08-03 21:50:44 -0300

    fix: Landing page responsiveness

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -11,36 +12,36 @@ import './styles.css';
 
 const Landing: React.FC = () => {
   return (
-  <div id="page-landing">
-    <div id="page-landing-container" className="container">
-      <div className="logo-container">
-        <img src={logoImg} alt="Proffy"/>
-        <h2>Sua plataforma de estudos online.</h2>
-      </div>
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy" />
+          <h2>Sua plataforma de estudos online.</h2>
+        </div>
 
-      <img 
-        src={landingImg} 
-        alt="Plataforma de estudos" 
-        className="hero-image"
-      />
+        <img
+          src={landingImg}
+          alt="Plataforma de estudos"
+          className="hero-image"
+        />
 
-      <div className="buttons-container">
-        <a href="" className="study">
-          <img src={studyIcon} alt="Estudar"/>
+        <div className="buttons-container">
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar" />
           Estudar
-        </a>
+        </Link>
 
-        <a href="" className="give-classes">
-          <img src={giveClassesIcon} alt="Dar aulas"/>
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" />
           Dar aulas
-        </a>
-      </div>
+        </Link>
+        </div>
 
-      <span className="total-connections">
-        Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
-      </span>
+        <span className="total-connections">
+          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+        </span>
+      </div>
     </div>
-  </div>
   )
 }
 
