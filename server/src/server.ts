27198f3f824e226815339153f57aba1ff2bbe3d5
Fernando Frascino <fernandofrascino@yahoo.com.br>
2020-08-05 09:05:14 -0300

    add: route to create classes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 
+import routes from './routes';
+
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get('/', (request, response) => {
-  return response.json({ sucess: true });
-});
 
 app.listen('3333');
